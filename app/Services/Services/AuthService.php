55f969ec60d4ba769f90bf243ef9e6bc311c07d2
csw <zobirofkir19@gmail.com>
2025-12-15 22:52:57 +0100

    create register service

--- app/Services/Services/AuthService.php
+++ app/Services/Services/AuthService.php
@@ -4,15 +4,29 @@ namespace App\Services\Services;
 
 use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\RegisterResource;
+use App\Models\User;
 use App\Services\Constructors\AuthConstructor;
 
 class AuthService implements AuthConstructor
 {
     /**
-     * Register New User
+     * Register a new user in the system
+     *
+     * - Validate user input through RegisterRequest
+     * - Create user record with hashed password (from model mutator)
+     * - Assign role selected by user
+     * - Return user details formatted in RegisterResource
      */
-    public function register(RegisterRequest $request) : RegisterResource
+    public function register(RegisterRequest $request): RegisterResource
     {
-        //
+        $createUser = User::create($request->validated());
+
+        if ($request->hasFile('avatar')) {
+            $path = $request->file('avatar')->store('avatars', 'public');
+            $createUser->avatar = $path;
+            $createUser->save();
+        }
+
+        return RegisterResource::make($createUser);
     }
 }
