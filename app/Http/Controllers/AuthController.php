7677ba2c86ee318234a3f4b7fde4454546536e11
csw <zobirofkir19@gmail.com>
2025-12-15 22:53:19 +0100

    add comments

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,12 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     /**
-     * Register New User
+     * Register a new user in the system
+     *
+     * - Validate user input through RegisterRequest
+     * - Create user record with hashed password (from model mutator)
+     * - Assign role selected by user
+     * - Return user details formatted in RegisterResource
      */
     public function register(RegisterRequest $request) : RegisterResource
     {
--- app/Services/Constructors/AuthConstructor.php
+++ app/Services/Constructors/AuthConstructor.php
@@ -8,7 +8,12 @@ use App\Http\Resources\RegisterResource;
 interface AuthConstructor
 {
     /**
-     * Register New User
+     * Register a new user in the system
+     *
+     * - Validate user input through RegisterRequest
+     * - Create user record with hashed password (from model mutator)
+     * - Assign role selected by user
+     * - Return user details formatted in RegisterResource
      */
     public function register(RegisterRequest $request) : RegisterResource;
 }
