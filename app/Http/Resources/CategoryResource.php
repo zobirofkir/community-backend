606c5ebcc1f86e03e8589b3cd31359a79c662503
csw <zobirofkir19@gmail.com>
2025-12-20 18:32:52 +0100

    create category resource

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -14,6 +14,11 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "title" => $this->title,
+            "description" => $this->description,
+            "created_at" => $this->created_at
+        ];
     }
 }
