56f98425694940903779b31a9ccf926eb9a65180
csw <zobirofkir19@gmail.com>
2025-12-20 06:25:36 +0100

    create posts route

--- routes/protected.php
+++ routes/protected.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /**
  * Categories routes
  */
 Route::apiResource('categories', CategoryController::class);
+
+
+/**
+ * Posts routes
+ */
+Route::apiResource('posts', PostController::class);
