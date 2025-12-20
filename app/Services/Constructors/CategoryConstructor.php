98b1afe8d497886f63bfc5cf5c65da092860f394
csw <zobirofkir19@gmail.com>
2025-12-20 06:10:33 +0100

    create post controller and model and description

--- app/Services/Constructors/CategoryConstructor.php
+++ app/Services/Constructors/CategoryConstructor.php
@@ -2,19 +2,7 @@
 
 namespace App\Services\Constructors;
 
-use App\Http\Resources\CategoryResource;
-use App\Models\Category;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-
 interface CategoryConstructor
 {
-    /**
-     * List all categories
-     */
-    public function index() : AnonymousResourceCollection;
-
-    /**
-     * Show specific category
-     */
-    public function show(Category $category) : CategoryResource;
+    //
 }
