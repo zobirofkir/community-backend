922cf0cb486a4a5f1753529803128f1139afdb2b
csw <zobirofkir19@gmail.com>
2025-12-20 05:59:10 +0100

    create category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+use App\Services\Constructors\CategoryConstructor;
+use App\Services\Facades\CategoryFacade;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
-class CategoryController extends Controller
+class CategoryController extends Controller implements CategoryConstructor
 {
-    //
+    /**
+     * List all categories
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return CategoryFacade::index();
+    }
+
+    /**
+     * show specific category
+     */
+    public function show(Category $category): CategoryResource
+    {
+        return CategoryFacade::show($category);
+    }
 }
