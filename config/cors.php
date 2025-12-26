fb302709bc3c26b6039187172763216d0658ad9e
csw <zobirofkir19@gmail.com>
2025-12-26 07:33:50 +0100

    update

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://community-frontend-self.vercel.app'],
+    'allowed_origins' => ['community-frontend-self.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
