d947b4e964ec5043b0fc4fe7f537f44dafaca8c5
csw <zobirofkir19@gmail.com>
2025-12-28 14:37:50 +0100

    create comment resource

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -14,6 +14,12 @@ class CommentResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "post_id" => $this->post_id,
+            "user_id" => $this->user_id,
+            "content" => $this->content,
+            "created_at" => $this->created_at,
+        ];
     }
 }
