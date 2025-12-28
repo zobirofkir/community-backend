12ef24afd889b75df5f11b303aa82823b764939a
csw <zobirofkir19@gmail.com>
2025-12-28 07:48:53 +0100

    create like request

--- app/Http/Requests/LikeRequest.php
+++ app/Http/Requests/LikeRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class LikeRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class LikeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,9 @@ class LikeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'post_id' => 'required|integer|exists:posts,id',
+
+            'type' => 'required',Rule::in(['like', 'dislike']),
         ];
     }
 }
