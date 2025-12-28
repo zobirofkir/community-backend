37c4288e20bd46db46d2a15f69265c2004acdc12
csw <zobirofkir19@gmail.com>
2025-12-28 07:52:06 +0100

    create like service

--- app/Services/Services/LikeService.php
+++ app/Services/Services/LikeService.php
@@ -4,12 +4,46 @@ namespace App\Services\Services;
 
 use App\Http\Requests\LikeRequest;
 use App\Http\Resources\LikeResource;
+use App\Models\Like;
 use App\Services\Constructors\LikeConstructor;
 
 class LikeService implements LikeConstructor
 {
     public function like(LikeRequest $request): LikeResource
     {
-        throw new \Exception('Not implemented');
+        $user = $request->user();
+        $postId = $request->post_id;
+        $type = $request->type; 
+
+        $reaction = Like::where('user_id', $user->id)
+            ->where('post_id', $postId)
+            ->first();
+
+        if ($reaction) {
+            if ($reaction->type === $type) {
+                $reaction->delete();
+
+                return new LikeResource([
+                    'type'   => null,
+                ]);
+            }
+            
+            /**
+             * disable (like <-> dislike)
+             */
+            $reaction->update([
+                'type' => $type,
+            ]);
+
+            return new LikeResource($reaction);
+        }
+
+        $reaction = Like::create([
+            'user_id' => $user->id,
+            'post_id' => $postId,
+            'type'    => $type,
+        ]);
+
+        return new LikeResource($reaction);
     }
 }
