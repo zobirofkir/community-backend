e7b123f971fe6851a2435eacb46ee96cf4255b62
csw <zobirofkir19@gmail.com>
2025-12-28 14:30:40 +0100

    create comment constructor

--- app/Services/Constructors/CommentConstructor.php
+++ app/Services/Constructors/CommentConstructor.php
@@ -2,7 +2,19 @@
 
 namespace App\Services\Constructors;
 
+use App\Http\Requests\CommentRequest;
+use App\Http\Resources\CommentResource;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
 interface CommentConstructor
 {
-    //
+    /**
+     * Add new comment
+     */
+    public function store(CommentRequest $request) : CommentResource;
+
+    /**
+     * List all comments
+     */
+    public function index() : AnonymousResourceCollection;
 }
