381c76396da0c1502aedf906609f7a7fa03fde92
csw <zobirofkir19@gmail.com>
2025-12-28 14:32:14 +0100

    create comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,10 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentRequest;
+use App\Http\Resources\CommentResource;
 use App\Services\Constructors\CommentConstructor;
+use App\Services\Facades\CommentFacade;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CommentController extends Controller implements CommentConstructor
 {
-    //
+    /**
+     * add new comment
+     */
+    public function store(CommentRequest $request): CommentResource
+    {
+        return CommentFacade::store($request);
+    }
+
+    /**
+     * List all comments
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return CommentFacade::index();
+    }
 }
--- app/Services/Services/CommentService.php
+++ app/Services/Services/CommentService.php
@@ -2,9 +2,26 @@
 
 namespace App\Services\Services;
 
+use App\Http\Requests\CommentRequest;
+use App\Http\Resources\CommentResource;
 use App\Services\Constructors\CommentConstructor;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CommentService implements CommentConstructor
 {
-    //
+    /**
+     * Add new comment
+     */
+    public function store(CommentRequest $request): CommentResource
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    /**
+     * List all comments
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        throw new \Exception('Not implemented');
+    }
 }
